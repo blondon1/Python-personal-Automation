a8c5bdef4318bbb256ae42ffcc08acf21f29e9bb
Bryan <bryanlondres5@gmail.com>
2023-09-03 11:02:57 -0400

    new temp path added

--- clean_bin.py
+++ clean_bin.py
@@ -6,6 +6,7 @@ import os
 import shutil
 
 winTempFiles = r'C:\Users\bryan\AppData\Local\Temp'
+winTemp2Files = r'C:\Windows\Temp'
 # Importing the required modules
 def job():    
     try:
@@ -30,6 +31,15 @@ def job():
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+    for filename in os.listdir(winTemp2Files):
+        file_path = os.path.join(winTemp2Files, filename)
+        try:
+         if os.path.isfile(file_path) or os.path.islink(file_path):
+             os.unlink(file_path)
+         elif os.path.isdir(file_path):
+             shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 schedule.every(10).seconds.do(job)
 #schedule.every().hour.do(job)
